0087988dd2a2d9d8e10f1f724272e2f6c0b6821d
AkashG23-AG <144423927+AkashG23-AG@users.noreply.github.com>
2023-09-08 12:35:29 +0530

    Add files via upload

---
+++ shared.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shared',
+  templateUrl: './shared.component.html',
+  styleUrls: ['./shared.component.scss'],
+  
+  
+})
+export class SharedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // dynName:string="Akash ghanate";
+  // StatName:string="Amol ghanate";
+  // myMethod(){
+  //   return "My Channel Name is " + this.dynName;
+  // }
+  // myMethod1(){
+  //   return "My Channel Name is " + this.StatName;
+  // }
+  // appStatus:boolean = true;
+  // status1: "Online" = "Online";
+  // status2: "Offline" = "Offline";
+
+  // msg:string ="";
+
+  // onAddCart(event:any){
+  //   this.msg= event.target.value + " Added in Cart";
+  // }
+  // close: boolean = false;
+  // openSettingtabpanel_taskSchedular() { 
+  
+  //   this.close = true;
+  // }
+  // closeSettingtabpanel_taskSchedular() {
+  //   this.close = false;
+   
+  // }
+
+// open: boolean = false;
+// openSidebarpanel() {
+
+//   this.open = true;
+// }
+// closeopenSidebarpanel() {
+//   this.open = false;
+ 
+// }
+// open: boolean = false;
+// openSidebarpanel() {
+
+//   this.open = true;
+// }
+// closeopenSidebarpanel() {
+//   this.open = false;
+ 
+// }
+   
+}
